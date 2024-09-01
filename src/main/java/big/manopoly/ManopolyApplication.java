9944828757bbff21eee26cdbc4c41223e039f1f6
Fares <fares.selwadii@gmail.com>
2024-09-01 20:07:23 +1200

    rewrote readme

---
+++ src/main/java/big/manopoly/ManopolyApplication.java
@@ -0,0 +1,13 @@
+package big.manopoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManopolyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManopolyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/big/manopoly/ManopolyApplicationTests.java
@@ -0,0 +1,13 @@
+package big.manopoly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManopolyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
