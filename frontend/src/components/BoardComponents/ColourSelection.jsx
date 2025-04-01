29e450fdfd700f8b0872038705caa7279d67f90f
farreeez <fares.selwadii@gmail.com>
2025-04-01 18:13:48 +1300

    finished colour selection style

--- frontend/src/components/BoardComponents/ColourSelection.jsx
+++ frontend/src/components/BoardComponents/ColourSelection.jsx
@@ -27,31 +27,32 @@ function chooseColour(colour, setPlayer) {
 function ColourSelection({ possibleColours, takenColours, player, setPlayer }) {
   return (
     <div className="modal">
-      <div className="overlay"></div>
-      <div className="modal-content">
-        <h1 className="chooseColour">Choose your colour</h1>
-        <div className="colours">
-          {possibleColours.map((colour) =>
-            !takenColours
-              .map((colour) => Number(colour.identifier))
-              .includes(colour.identifier) ? (
-              <div
-                className="circle"
-                onClick={() => chooseColour(colour, setPlayer)}
-                style={{
-                  backgroundColor: `rgb(${colour.red},${colour.green},${colour.blue})`,
-                }}
-              ></div>
-            ) : (
-              <div
-                className="circle"
-                style={{
-                  backgroundColor: `rgb(${colour.red},${colour.green},${colour.blue})`,
-                  opacity: 0.2,
-                }}
-              ></div>
-            )
-          )}
+      <div className="overlay">
+        <div className="modal-content">
+          <h1 className="chooseColour">Choose your colour</h1>
+          <div className="colours">
+            {possibleColours.map((colour) =>
+              !takenColours
+                .map((colour) => Number(colour.identifier))
+                .includes(colour.identifier) ? (
+                <div
+                  className="circle"
+                  onClick={() => chooseColour(colour, setPlayer)}
+                  style={{
+                    backgroundColor: `rgb(${colour.red},${colour.green},${colour.blue})`,
+                  }}
+                ></div>
+              ) : (
+                <div
+                  className="circle"
+                  style={{
+                    backgroundColor: `rgb(${colour.red},${colour.green},${colour.blue})`,
+                    opacity: 0.2,
+                  }}
+                ></div>
+              )
+            )}
+          </div>
         </div>
       </div>
     </div>
