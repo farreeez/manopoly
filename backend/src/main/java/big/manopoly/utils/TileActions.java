cbba434a2c6b4205f86579bc33dd324052bc4902
farreeez <fares.selwadii@gmail.com>
2025-04-27 12:11:42 +1200

    added conditions where a player lands on another player's property but does not have to pay rent

--- backend/src/main/java/big/manopoly/utils/TileActions.java
+++ backend/src/main/java/big/manopoly/utils/TileActions.java
@@ -13,55 +13,58 @@ public class TileActions {
     // accordingly
     public static TileActionDTO conductTileAction(Player player, Board board, int[] diceRolls) throws Exception {
         BoardSquare square = board.getSquares().get(player.getPosition().getPosition());
-        if(square instanceof Property) {
+        if (square instanceof Property) {
             Property property = (Property) square;
             return conductPropertyAction(player, board, property, diceRolls);
-        } else if(square instanceof NotProperty){
+        } else if (square instanceof NotProperty) {
             NotProperty notProperty = (NotProperty) square;
             return conductNonPropertyAction(player, board, notProperty, diceRolls);
         } else {
-            throw new Error("BoardSquare of invalid class type in conduct tile action in big.manopoly.utils.TileActions.java");
+            throw new Error(
+                    "BoardSquare of invalid class type in conduct tile action in big.manopoly.utils.TileActions.java");
         }
 
     }
 
-    public static TileActionDTO conductPropertyAction(Player player, Board board, Property property, int[] diceRolls) throws Exception {
+    public static TileActionDTO conductPropertyAction(Player player, Board board, Property property, int[] diceRolls)
+            throws Exception {
         Player owner = property.getOwner();
 
-        if(owner == null){
+        if (owner == null) {
             // card owned by the bank ask player if they want to purchase it
             return new CardPurchaseActionDTO(diceRolls, property.getPrice());
-        } else if (owner == player) {
-            // player owns card do nothing and return empty actionDTO
-            return new TileActionDTO(diceRolls);
-        } else {
-            // other player owns property pay rent.
+        }
 
-            // case player has rent money.
+        if (owner != player && !property.isMortgaged() && property.getOwner().isFree()) {
             // TODO: finish negative balance functionality.
             // TODO: display all transactions in real time to all players
             int rent = property.getRent();
 
-            if(rent < 0) {
-                throw new Exception("rent money should not be negative in conductPropertyAction in big.manopoly.utils.TileActions");
+            if (rent < 0) {
+                throw new Exception(
+                        "rent money should not be negative in conductPropertyAction in big.manopoly.utils.TileActions");
             }
 
             player.setMoney(player.getMoney() - rent);
 
-            // if player cant afford the rent and has a negative balance only transfer the amount that they can afford to the owner
-            if(player.getMoney() < 0) {
-                // this is so that the owner does not receive money that doesn't exist yet. (player.getMoney() is negative at this stage.)
+            // if player cant afford the rent and has a negative balance only transfer the
+            // amount that they can afford to the owner
+            if (player.getMoney() < 0) {
+                // this is so that the owner does not receive money that doesn't exist yet.
+                // (player.getMoney() is negative at this stage.)
                 rent += player.getMoney();
             }
 
             owner.setMoney(owner.getMoney() + rent);
-
-            return new TileActionDTO(diceRolls);
         }
+
+        return new TileActionDTO(diceRolls);
+
     }
 
     // TODO: finish this once chance card and jail functionality is complete.
-    public static TileActionDTO conductNonPropertyAction(Player player, Board board, NotProperty notProperty, int[] diceRolls) {
+    public static TileActionDTO conductNonPropertyAction(Player player, Board board, NotProperty notProperty,
+            int[] diceRolls) {
         return new TileActionDTO(diceRolls);
     }
 }
